b8020b77aed7a19372ae1ba794af3440143dc862
Mark <mark.jones1@sydney.edu.au>
2023-11-10 13:54:03 +0800

    Updated figs for gh101

--- resources/fundamentals/random-numbers.R
+++ resources/fundamentals/random-numbers.R
@@ -1,11 +1,11 @@
 library(data.table)
 
 N <- 100
-x <- sample(-3:3, size = N, replace = T)
-e <- rnorm(N, 0, 1)
-g <- rgamma(N, 1, 1)
-p <- rpois(N, 10)
 
-d <- data.table(x = x, e = e, g = g, p = p)
+d <- data.table(
+  categorical = sample(-3:3, size = N, replace = T),
+  normal = rnorm(N, 0, 1),
+  gamma = rgamma(N, 1, 1),
+  poisson = rpois(N, 10))
 
 apply(d, 2, summary)
